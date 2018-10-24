eb80b7179e036d6d9a38a05aa2e7121c124ddcc6
Jonathan Monroe <jonathan0monroe@gmail.com>
2018-10-23 23:58:33 -0500

    Added hard dataset
    And tuned mischievous dataset

--- clusteringProject/clustering.py
+++ clusteringProject/clustering.py
@@ -6,17 +6,17 @@ Created on Sat Oct 20 10:41:13 2018
 """
 import numpy as np
 import matplotlib.pyplot as plt
-np.random.seed(2016)
+np.random.seed(2047)
 
 def get_easy_data(show=False):
     '''
     Creates two well-separated 2-d clusters 
     '''
-    mu1 = [3,9]
-    mu2 = [6,4]
-    mu3 = [2,2]
-    sig = 0.4
-    numPoints = 200 ## magic at 1,2,12, and 16 (checked to 20)
+    mu1 = [0.2,2.5]
+    mu2 = [2,2]
+    mu3 = [1,1]
+    sig = 0.15
+    numPoints = 200 ## magic at 1,2,12, and 16 (checked to 20); 2016 seed
     cluster1 = sig*np.random.randn(numPoints ,2) + mu1
     cluster2 = sig*np.random.randn(numPoints ,2) + mu2
     cluster3 = sig*np.random.randn(numPoints ,2) + mu3
@@ -30,6 +30,29 @@ def get_easy_data(show=False):
     return cluster1, cluster2, cluster3
 ##END get_easy_data
     
+    
+def get_hard_data(show=False):
+    '''
+    Creates two well-separated 2-d clusters 
+    '''
+    mu1 = [0.2,2.5]
+    mu2 = [2,2]
+    mu3 = [1,1]
+    sig1,sig2, sig3 = 0.15, 0.15, 0.4
+    numPoints = 80 ## magic at 1,2,12, and 16 (checked to 20)
+    cluster1 = sig1*np.random.randn(numPoints,2) + mu1
+    cluster2 = sig2*np.random.randn(numPoints,2) + mu2
+    cluster3 = sig3*np.random.randn(5*numPoints,2) + mu3
+    
+    
+    if show:
+        plt.scatter(cluster1[:,0], cluster1[:,1], color='cyan')
+        plt.scatter(cluster2[:,0], cluster2[:,1], color='orange')
+        plt.scatter(cluster3[:,0], cluster3[:,1], color='purple')
+        plt.show()
+        
+    return cluster1, cluster2, cluster3
+##END get_hard_data    
 
 def calc_distance(xy_tuple,centers_list):
     ## make a separate function for extensibility
@@ -45,22 +68,22 @@ def k_means(data, k):
     data:   (n, dim) array 
     k:      number of clusters
     '''
+    k = min(k,4) ## this is how many colors I've chosen...
     data = np.array(data)
     if len(data.shape)>1:
         dim = data.shape[1]
     else:
         dim = 1
     numPoints = data.shape[0]
-    color_list = ["cyan","orange","purple"]
+    color_list = ["cyan","orange","purple","green"]
     
-    num_iter = 3
+    num_iter = 4
     centers = np.zeros((k,dim))
     cluster_counts = np.zeros(k)
     cluster_ids_fullList = np.zeros((num_iter+1, numPoints) ,dtype="int")    
     distance_fullList = np.zeros(num_iter+1)
     
     cluster_indices = np.random.randint(0,k,size=numPoints)
-    cluster_indices[0]=2    
     cluster_ids_fullList[0] = cluster_indices
 
     ## Initial calculations
@@ -121,7 +144,8 @@ def k_means(data, k):
 
 def main():
     ## get data
-    c1, c2, c3 = get_easy_data()
+    c1, c2, c3 = get_easy_data(True)
+    #c1,c2,c3 = get_hard_data(True)
     
     ## shuffle data
     xs = list(c1[:, 0]) + list(c2[:,0]) + list(c3[:,0])
@@ -134,4 +158,12 @@ def main():
 
 
 if __name__ == '__main__':
-    foo, bar= main()
+    foo = main()
+    
+'''
+    mu1 = [3,9]
+    mu2 = [6,4]
+    mu3 = [2,2]
+    sig = 0.4
+    numPoints = 200 ## magic at 1,2,12, and 16 (checked to 20); 2016 seed
+'''

eedac4d40c67bb85e66345b7a08d6d83936e2d32
John <jonathan0monroe@gmail.com>
2018-11-14 17:25:34 -0600

    added stuff

--- hw8/analyze_neuralRecordings.py
+++ hw8/analyze_neuralRecordings.py
@@ -16,13 +16,7 @@ from scipy import cluster as clust
 
 
 
-def test_stuff():
-   read_dict = io.loadmat("bint_fishmovie32_100.mat")
-   data = read_dict['bint']
-   
-   ## look at data
-   # data is 297 x 160 x 953
-   # corresponding to 297 iterations of 160 neurons for 953 time steps
+def test_stuff(data):
    average_iter = np.mean(data, axis=0)
    if False:
        plt.figure()
@@ -71,11 +65,7 @@ def test_stuff():
 ##END test_stuff
    
 
-def show_allTraces():
-    read_dict = io.loadmat("bint_fishmovie32_100.mat")
-    data = read_dict['bint']
-    # data is 297 x 160 x 953
-    # corresponding to 297 iterations of 160 neurons for 953 time steps
+def show_allTraces(show=False):
    
     traceOut_iter = np.mean(data,axis=0) ## cells across time
     #print(traceOut_iter.shape)
@@ -94,11 +84,79 @@ def show_allTraces():
     plt.figure()
     plt.imshow(traceOut_cells)
     plt.ylabel("iteration"); plt.xlabel("time")
+
+    if show: plt.show()
 ##END show_allTraces()
+
+def corr(x1, x2, num_avg, max_tau,offset=0):
+    ## x1 needs to be at least num_avg + max_tau
+    ## x2 needs to be at least num_avg
+    out = np.zeros(max_tau)
+    for i in range(max_tau):
+        pairwise_product = x1[offset+i:num_avg+i] * x2[offset:num_avg]
+        out[i] = sum(pairwise_product) 
+    return out
+##END corr
+  
+
+def cf_rank(data):
+    avg_over_runs = np.mean(data,axis=0)
+    #plt.imshow(avg_over_runs)
+
+    def avg_first_n(xs, n=200):
+        return np.mean(xs[710:730])
+
+    sorted_avg = list(avg_over_runs)
+    sorted_avg.sort(key=avg_first_n, reverse=True)
+    sorted_avg= np.array(sorted_avg)
+
+    ## show sorted list
+    plt.figure()
+    plt.imshow(sorted_avg)
+    plt.xlabel("Time step")
+    plt.ylabel("Neuron index (sorted")
+
+    ## measure some correlations
+    plt.figure()
+    num_neurons = sorted_avg.shape[0]
+    corr_image = np.zeros((num_neurons,30))
+    base = sorted_avg[0]
+    for i,neuron in enumerate(sorted_avg):
+        line = corr(base, neuron, num_avg=20,max_tau = 30, offset=0)
+        corr_image[i] = line
     
+    plt.imshow(corr_image)
+    plt.show()
+## cf_rank
+
+
+def regional_fft(data):
+    avg_over_runs = np.mean(data,axis=0)
+
+    ## cut out region 
+    region_min = 125
+    region_max = 325
+    sliced = avg_over_runs[:, region_min:region_max]
+    print(sliced.shape)
+    plt.figure()
+    plt.imshow(sliced)
+
+    plt.figure()
+    for neuron in sliced[:5]:
+        plt.plot( neuron )
+    plt.show()
+##END regional_fft
+ 
+ 
 def main():
-    return 0;
-    show_allTraces()
+    read_dict = io.loadmat("bint_fishmovie32_100.mat")
+    data = read_dict['bint']
+    # data is 297 x 160 x 953
+    # corresponding to 297 iterations of 160 neurons for 953 time steps
+
+    #show_allTraces(data, True)
+    #cf_rank(data)    
+    regional_fft(data)
 ##END main
     
 
